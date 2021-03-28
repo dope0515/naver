e270b49674eb661fcc1a1549cecdd789a1cc1cbd
dope0515 <61546301+dope0515@users.noreply.github.com>
2021-03-28 17:56:26 +0900

    Add files via upload

--- jquery/change_ad.js
+++ jquery/change_ad.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var $randomNum = Math.floor(Math.random()*3+1);
     var $adContent = $('.ad_content:nth-child('+$randomNum+')');
     function showAd(){
-        $adContent.css('display','block')
-    }
+        $adContent.attr('class','show_ad');
+    };
     showAd();
 });
